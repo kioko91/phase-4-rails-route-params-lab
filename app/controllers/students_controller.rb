2fe2bf6b5d477c90b451e81307542f2e5ed58d4a
Calvin <calvinkioko91@gmail.com>
2022-09-28 11:55:43 +0300

    commit tree 4a5f336820780a5c4712a6f0ebe3dd0771c70fa2

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,16 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    
     render json: students
+    
   end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  
 
 end
